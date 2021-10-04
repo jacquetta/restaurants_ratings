321f4d29a4919948dd511d0917d1435ead82699f
jacquetta <jacquettavb@hotmail.com>
2021-10-04 14:37:26 -0500

    Added validating new score

--- ratings.py
+++ ratings.py
@@ -8,17 +8,23 @@ def ratings_res(file):
     for rating in ratings:
         rating = rating.rstrip()
         lines  = rating.split(":")
-        for line in lines:
-            rating_restaurants[lines[0]] = lines[1]
+        rating_restaurants[lines[0]] = lines[1]
     ratings.close()
     key_res = sorted(rating_restaurants.items())
     for name in key_res:
         print(f"{name[0]} is rated at {name[1]}.")
     prompt_res = input('Enter Restaurant Name: ')
-    prompt_rating = input('Enter Restaurant Rating: ')
-    rating_restaurants[prompt_res] = prompt_rating
-    updated_list = sorted(rating_restaurants.items())
-    for name in updated_list:
-        print(f"{name[0]} is rated at {name[1]}.")
-
+    prompt_rating = int(input('Enter Restaurant Rating: '))
+    if prompt_rating > 0 and prompt_rating <= 5:
+        rating_restaurants[prompt_res] = prompt_rating
+        updated_list = sorted(rating_restaurants.items())
+        for name in updated_list:
+            print(f"{name[0]} is rated at {name[1]}.")
+    else:
+        print("Invalid number.")
+        prompt_rating = input("Enter a new number between 1 and 5: ")
+        rating_restaurants[prompt_res] = prompt_rating
+        updated_list = sorted(rating_restaurants.items())
+        for name in updated_list:
+            print(f"{name[0]} is rated at {name[1]}.")
 ratings_res("scores.txt")
